9b7a81f31ffb1d976ea2b0f8be1f9d97c6d93dce
Stian Prestholdt <stian@e5r.no>
2019-10-30 14:11:51 +0100

    Update Puppeteer to v1.19.0 and Koa to 2.11.0
    Since we now use alpine image for docker, we pin puppeteer to v1.19.0 as
    it's recommended in Puppeteer docs.
    
    By bumping Koa to v2 we had to update the code to use async/await.
    
    A minor bug fix for the environment `SCREENIE_SCREENSHOT_DELAY` which now
    uses regular `||` instead of the bitwise-or `|` syntax.

--- src/server.js
+++ src/server.js
@@ -1,14 +1,17 @@
 #!/usr/bin/env node
 
-const fs = require('fs');
 const Koa = require('koa');
-const puppeteer = require('puppeteer');
-const tmp = require('tmp');
 const winston = require('winston');
 const createPuppeteerPool = require('puppeteer-pool').default;
+const { combine, timestamp, printf } = winston.format;
 
-const logger = new winston.Logger({
+const loggerFormat = printf(({ message, timestamp }) => {
+  return `[${timestamp}] ${message}`;
+});
+
+const logger = winston.createLogger({
   level: process.env.SCREENIE_LOG_LEVEL || 'info',
+  format: combine(timestamp(), loggerFormat),
   transports: [
     new winston.transports.Console({
       timestamp: () => new Date().toISOString(),
@@ -16,7 +19,7 @@ const logger = new winston.Logger({
   ],
 });
 
-logger.verbose('Setting up defaults from environment');
+logger.log('verbose', 'Setting up defaults from environment');
 const chromiumArgs = process.env.SCREENIE_CHROMIUM_ARGS
   ? { args: process.env.SCREENIE_CHROMIUM_ARGS.split(' ') }
   : {};
@@ -33,7 +36,7 @@ const supportedFormats = ['jpg', 'jpeg', 'pdf', 'png'];
 const allowFileScheme = process.env.SCREENIE_ALLOW_FILE_SCHEME || false;
 
 const app = new Koa();
-logger.verbose('Created KOA server');
+logger.log('verbose', 'Created KOA server');
 
 const pool = createPuppeteerPool({
   min: process.env.SCREENIE_POOL_MIN || 2,
@@ -43,17 +46,17 @@ const pool = createPuppeteerPool({
 
 const screenshotDelay = () =>
   new Promise(resolve =>
-    setTimeout(resolve, process.env.SCREENIE_SCREENSHOT_DELAY | 50)
+    setTimeout(resolve, process.env.SCREENIE_SCREENSHOT_DELAY || 50)
   );
 
-logger.verbose('Created Puppeteer pool');
+logger.log('verbose', 'Created Puppeteer pool');
 
 /*
  * Clean up the Puppeteer pool before exiting when receiving a termination
  * signal. Exit with status code 143 (128 + SIGTERM's signal number, 15).
  */
 process.on('SIGTERM', () => {
-  logger.info('Received SIGTERM, exiting...');
+  logger.log('info', 'Received SIGTERM, exiting...');
   pool
     .drain()
     .then(() => pool.clear())
@@ -63,41 +66,44 @@ process.on('SIGTERM', () => {
 /**
  * Set up a Puppeteer instance for a page and configure viewport size.
  */
-app.use(function*(next) {
-  const { width, height } = this.request.query;
+app.use(async (ctx, next) => {
+  const { width, height } = ctx.request.query;
   const size = {
     width: Math.min(2048, parseInt(width, 10) || imageSize.width),
     height: Math.min(2048, parseInt(height, 10) || imageSize.height),
   };
   let pageError;
 
-  logger.verbose(`Instantiating Page with size ${size.width}x${size.height}`);
+  logger.log(
+    'verbose',
+    `Instantiating Page with size ${size.width}x${size.height}`
+  );
 
-  yield pool.use(instance => {
+  await pool.use(instance => {
     const pid = instance.process().pid;
-    logger.verbose(`Using browser instance with PID ${pid}`);
+    logger.log('verbose', `Using browser instance with PID ${pid}`);
     return instance
       .newPage()
       .then(page => {
-        logger.verbose('Set page instance on state');
-        this.state.page = page;
+        logger.log('verbose', 'Set page instance on state');
+        ctx.state.page = page;
       })
       .then(() => {
-        logger.verbose('Set viewport for page');
-        return this.state.page.setViewport(size);
+        logger.log('verbose', 'Set viewport for page');
+        return ctx.state.page.setViewport(size);
       })
       .catch(error => {
         pageError = error;
-        logger.verbose(`Invalidating instance with PID ${pid}`);
+        logger.log('verbose', `Invalidating instance with PID ${pid}`);
         pool.invalidate(instance);
       });
   });
 
   if (pageError) {
-    this.throw(400, `Could not open a page: ${pageError.message}`);
+    ctx.throw(400, `Could not open a page: ${pageError.message}`);
   }
 
-  yield next;
+  await next();
 });
 
 /**
@@ -106,32 +112,32 @@ app.use(function*(next) {
  * Throws 400 Bad Request if no URL is provided, and 404 Not Found if
  * Puppeteer could not load the URL.
  */
-app.use(function*(next) {
-  const { page } = this.state;
-  const { url } = this.request.query;
+app.use(async (ctx, next) => {
+  const { page } = ctx.state;
+  const { url } = ctx.request.query;
 
   let gotoError;
 
   if (!url) {
-    this.throw(400, 'No url request parameter supplied.');
+    ctx.throw(400, 'No url request parameter supplied.');
   }
 
   if (url.indexOf('file://') >= 0 && !allowFileScheme) {
-    this.throw(403);
+    ctx.throw(403);
   }
 
-  logger.verbose(`Attempting to load ${url}`);
+  logger.log('verbose', `Attempting to load ${url}`);
 
-  yield page
+  await page
     .goto(url)
     .then(screenshotDelay)
     .catch(() => (gotoError = true));
 
   if (gotoError) {
-    this.throw(404);
+    ctx.throw(404);
   }
 
-  yield next;
+  await next();
 });
 
 /**
@@ -140,16 +146,16 @@ app.use(function*(next) {
  * The format must be among the formats supported by Puppeteer, else 400
  * Bad Request is thrown. If no format is provided, the default is used.
  */
-app.use(function*(next) {
-  const { format = defaultFormat } = this.request.query;
+app.use(async (ctx, next) => {
+  const { format = defaultFormat } = ctx.request.query;
 
   if (supportedFormats.indexOf(format.toLowerCase()) === -1) {
-    this.throw(400, `Format ${format} not supported.`);
+    ctx.throw(400, `Format ${format} not supported.`);
   }
 
-  this.type = this.state.format = format;
+  ctx.type = ctx.state.format = format;
 
-  yield next;
+  await next();
 });
 
 /**
@@ -157,40 +163,48 @@ app.use(function*(next) {
  *
  * If successful the screenshot is sent as the response.
  */
-app.use(function*(next) {
-  const { url, fullPage } = this.request.query;
-  const { format, page, browser } = this.state;
+app.use(async (ctx, next) => {
+  const { url, fullPage } = ctx.request.query;
+  const { format, page } = ctx.state;
   const { width, height } = page.viewport();
   let renderError;
 
-  logger.info(`Rendering screenshot of ${url} to ${format}`);
+  logger.log('info', `Rendering screenshot of ${url} to ${format}`);
 
   if (format === 'pdf') {
-    yield page
+    await page
       .pdf({
         format: 'A4',
         margin: { top: '1cm', right: '1cm', bottom: '1cm', left: '1cm' },
       })
-      .then(response => (this.body = response))
+      .then(response => (ctx.body = response))
       .catch(error => (renderError = error));
   } else {
-    let clipInfo = fullPage === "1" ? {fullPage: true} : { clip: { x: 0, y: 0, width: width, height: height}} ;
-    yield page
-      .screenshot(Object.assign({
-        type: format === 'jpg' ? 'jpeg' : format,
-        omitBackground: true,
-      }, clipInfo))
-      .then(response => (this.body = response))
+    let clipInfo =
+      fullPage === '1'
+        ? { fullPage: true }
+        : { clip: { x: 0, y: 0, width: width, height: height } };
+    await page
+      .screenshot(
+        Object.assign(
+          {
+            type: format === 'jpg' ? 'jpeg' : format,
+            omitBackground: true,
+          },
+          clipInfo
+        )
+      )
+      .then(response => (ctx.body = response))
       .catch(error => (renderError = error));
   }
 
   if (renderError) {
-    this.throw(400, `Could not render page: ${renderError.message}`);
+    ctx.throw(400, `Could not render page: ${renderError.message}`);
   }
 
-  yield page.close();
+  await page.close();
 
-  yield next;
+  await next();
 });
 
 /**
@@ -203,9 +217,8 @@ app.on('error', (error, context) => {
     page.close();
   }
 
-  logger.error(error.message);
+  logger.log('error', error.message);
 });
 
 app.listen(serverPort);
-logger.info(`Screenie server started on port ${serverPort}`);
-
+logger.log('info', `Screenie server started on port ${serverPort}`);
